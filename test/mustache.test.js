cb535912bb2a144e17c978f5bc634ad7a3a3bd16
Ahmad Fauzan <fauzan18296@gmail.com>
2024-06-22 14:54:04 +0700

    add: Menyelesaikan materi nodeJs bab mustache

--- test/mustache.test.js
+++ test/mustache.test.js
@@ -1,5 +1,6 @@
-import Mustache from "mustache";
+import Mustache, { name } from "mustache";
 import fs from "fs/promises";
+import { title } from "process";
 
 test("Menggunakan Mustache", () => {
   const data = Mustache.render("Hello {{name}}", {name:"Fauzan"})
@@ -80,4 +81,78 @@ test('Inverted Sections', async () => {
   const data = Mustache.render(helloTemplate, {})
   console.log(data)
   expect(data).toContain("Hello Guest")
+})
+
+test('List', async () => {
+  const helloTemplate = await fs.readFile("./templates/hobbies.mustache")
+  .then(data => data.toString())
+  const data = Mustache.render(helloTemplate, {
+    hobbies:["Coding", "Gaming", "Reading"]
+  })
+  console.log(data)
+  expect(data).toContain("Coding")
+  expect(data).toContain("Gaming")
+  expect(data).toContain("Reading")
+})
+
+test('List Object', async () => {
+  const helloTemplate = await fs.readFile("./templates/students.mustache")
+  .then(data => data.toString())
+  const data = Mustache.render(helloTemplate, {
+    students: [{
+      name:'Ahmad Fauzan', value: 100
+    },
+    {
+      name: 'Achmad Ravi Firdaus', value:90
+    }]
+  })
+  console.log(data)
+  expect(data).toContain("Ahmad Fauzan")
+  expect(data).toContain("Achmad Ravi Firdaus")
+  expect(data).toContain("100")
+  expect(data).toContain("90")
+})
+
+test('Function', async () => {
+  const parameter = {
+   name: 'Ahmad Fauzan',
+   upper: () => {
+    return (text,render) => {
+      return render(text).toUpperCase();
+    }
+   }
+  }
+ const data = Mustache.render("Hello {{#upper}}{{name}}{{/upper}}", parameter)
+  console.log(data)
+  expect(data).toBe("Hello AHMAD FAUZAN")
+})
+
+test('Comment', async () => {
+  const helloTemplate = await fs.readFile("./templates/comment.mustache")
+  .then(data => data.toString())
+  const data = Mustache.render(helloTemplate, {
+    title: "NodeJs"
+  })
+  console.log(data)
+  expect(data).toContain("NodeJs")
+})
+
+test('Partial', async () => {
+  const headerTemplate = await fs.readFile("./templates/header.mustache").then(data => data.toString())
+  const footerTemplate = await fs.readFile("./templates/footer.mustache").then(data => data.toString())
+  const contentTemplate = await fs.readFile("./templates/content.mustache").then(data => data.toString())
+  
+  const data = Mustache.render(contentTemplate, {
+    title: 'Belajar Partial',
+    content: 'Ahmad Fauzan'
+  },
+  {
+  header:headerTemplate,
+  footer:footerTemplate
+  }
+)
+  console.log(data)
+  expect(data).toContain("Belajar Partial")
+  expect(data).toContain("Ahmad Fauzan")
+  expect(data).toContain("Powered by AF")
 })
